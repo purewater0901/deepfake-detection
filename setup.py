e397e5e37b44f36f9916feae82c9c21fab0ac82f
yutaka <purewater0901@gmail.com>
2025-04-08 10:55:03 -0700

    add setup.py
    Signed-off-by: yutaka <purewater0901@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='deepfake_detection',
+    version='0.1.0',
+    description='A simple calculator package for basic arithmetic operations',
+    author='Yutaka',
+    author_email='purewater0901@gmail.com',
+    url='git@github.com:purewater0901/deepfake-detection.git',
+    packages=find_packages(),
+    python_requires='>=3.10',
+)
