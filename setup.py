bad3715340d4a9e0cd5f03b0d79ab032e7b5c371
yutaka <purewater0901@gmail.com>
2025-04-09 12:43:20 -0700

    update setup.py
    Signed-off-by: yutaka <purewater0901@gmail.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+def load_requirements(filename="requirements.txt"):
+    with open(filename, encoding="utf-8") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
 setup(
     name='deepfake_detection',
     version='0.1.0',
@@ -9,4 +13,5 @@ setup(
     url='git@github.com:purewater0901/deepfake-detection.git',
     packages=find_packages(),
     python_requires='>=3.10',
+    install_requires=load_requirements("requirements.txt"),
 )
